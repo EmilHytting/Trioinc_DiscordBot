6c9bb869ffe8834fe7a9fd7f6a2f185a4f260378
Emil Hytting <emilmm21@gmail.com>
2024-08-22 08:59:34 +0200

    Første Push - Repo creation
    www

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace TrioincBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var token = Environment.GetEnvironmentVariable("DISCORD TOKEN");
+        }
+    }
+}
